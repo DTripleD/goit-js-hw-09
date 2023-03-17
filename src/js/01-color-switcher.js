91d2adb19d296b0f6a55f65940d89a1ae8ff4e10
DTripleD <DTripleD>
2023-03-17 20:36:43 +0200

    make 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,35 +1,37 @@
-
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
-
-const startBtn = document.querySelector("[data-start]");
-const stopBtn = document.querySelector("[data-stop]");
-const background = document.querySelector("body");
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const background = document.querySelector('body');
 
 let intervalID = 0;
 let canIClick = true;
 
-startBtn.addEventListener("click", forAddingBgColor)
+startBtn.addEventListener('click', forAddingBgColor);
 
+stopBtn.addEventListener('click', forStopChangingColor);
 
-stopBtn.addEventListener("click", forStopChangingColor);
+function forAddingBgColor() {
+  if (canIClick) {
+    // background.style.backgroundColor = getRandomHexColor();
+    const changeBgColor = setInterval(() => {
+      background.style.backgroundColor = getRandomHexColor();
+    }, 1000);
 
-
-function forAddingBgColor(){
-    if(canIClick){
-        const changeBgColor = setInterval(() => {
-            background.style.backgroundColor = getRandomHexColor();
-        }, 1000);
-    
-        intervalID = changeBgColor;
-        canIClick = false;
-    }
+    intervalID = changeBgColor;
+    canIClick = false;
+    stopBtn.style.opacity = '0.5';
+    startBtn.style.opacity = '1';
+  }
 }
 
-function forStopChangingColor(){
-    clearInterval(intervalID);
-    canIClick = true;
+function forStopChangingColor() {
+  clearInterval(intervalID);
+  canIClick = true;
+  stopBtn.style.opacity = '1';
+  startBtn.style.opacity = '0.5';
 }
-
