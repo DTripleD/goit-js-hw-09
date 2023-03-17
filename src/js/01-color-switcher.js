27e643c511d521039a5729a8038463738e9077cb
DTripleD <DTripleD>
2023-03-18 00:01:36 +0200

    end 2 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,29 +9,52 @@ const stopBtn = document.querySelector('[data-stop]');
 const background = document.querySelector('body');
 
 let intervalID = 0;
-let canIClick = true;
 
 startBtn.addEventListener('click', forAddingBgColor);
 
 stopBtn.addEventListener('click', forStopChangingColor);
 
 function forAddingBgColor() {
-  if (canIClick) {
-    // background.style.backgroundColor = getRandomHexColor();
-    const changeBgColor = setInterval(() => {
-      background.style.backgroundColor = getRandomHexColor();
-    }, 1000);
+  // background.style.backgroundColor = getRandomHexColor();
+  const changeBgColor = setInterval(() => {
+    background.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 
-    intervalID = changeBgColor;
-    canIClick = false;
-    stopBtn.style.opacity = '0.5';
-    startBtn.style.opacity = '1';
-  }
+  intervalID = changeBgColor;
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled', '');
 }
 
 function forStopChangingColor() {
   clearInterval(intervalID);
-  canIClick = true;
-  stopBtn.style.opacity = '1';
-  startBtn.style.opacity = '0.5';
+
+  stopBtn.setAttribute('disabled', '');
+  startBtn.removeAttribute('disabled', '');
 }
+
+// 2 variant
+
+// let canIClick = true;
+
+// function forAddingBgColor() {
+//   if (canIClick) {
+//     // background.style.backgroundColor = getRandomHexColor();
+//     const changeBgColor = setInterval(() => {
+//       background.style.backgroundColor = getRandomHexColor();
+//     }, 1000);
+
+//     intervalID = changeBgColor;
+//     canIClick = false;
+//     stopBtn.style.opacity = "0.5";
+//     startBtn.style.opacity = "1"
+
+//   }
+// }
+
+// function forStopChangingColor() {
+//   clearInterval(intervalID);
+//   canIClick = true;
+//   stopBtn.style.opacity = "1";
+//   startBtn.style.opacity = "0.5"
+// }
+
